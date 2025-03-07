9b4a61f85318966be3336c1268201f512948d17d
amayajavier37 <javigamer84@gmail.com>
2025-03-07 18:23:49 -0300

    Cambio en el comentario para explicar las clases anemicas y no anemicas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,12 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        //La diferencia entre objtos anemicos y no anemicos es que los anemicos
+        // La diferencia entre las clases anemicas y no anemicas es que los anemicos
         // solo funcionan como clases que almacenan datos y que solo tienen
         // como metodos los de acceso y modificacion de esos datos.
         // En cambio, los no anemicos tiene más funcionalidades y métodos que corresponden
-        //  a la logica del negocio, como por ejemplo verificar, validar etc.
+        // a la logica del negocio, como por ejemplo verificar, validar etc.
+        // Haciendo más facil la lectura y mantenimiento del código.
 
         System.out.println("Tiempo con clase no anemica");
         var hoy = new TiempoNoAnemico();
@@ -26,5 +27,6 @@ public class Main {
         SimpleDateFormat formatoCorto = new SimpleDateFormat("dd/MM/yyyy");
         String formatCorto = formatoCorto.format(fecha);
         System.out.println(formatCorto);
+        
     }
 }
